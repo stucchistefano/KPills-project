8c6f1e8eb5d57f42b98d581c3c2fd8025f9f5e7e
stucchistefano <stefano4.stucchi@mail.polimi.it>
2025-04-10 15:50:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Ciao!"
 )
